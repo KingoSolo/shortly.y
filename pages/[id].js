1f5587ac43d39ec0187d300b15e9ffbfcb2485c1
Solomon <soluwaseun806@gmail.com>
2024-06-14 00:49:36 +0100

    db queried

---
+++ pages/[id].js
@@ -0,0 +1,44 @@
+import { supabase } from '../../utils/supabase'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+const ShortUrlRedirect = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  useEffect(() => {
+    const redirectToOriginalUrl = async () => {
+      const { data, error } = await supabase
+        .from('links')
+        .select('url,views')
+        .eq('id', id)
+        .single()
+        
+      if (error) {
+        console.error('Error fetching original URL:', error)
+        return
+      }
+
+      const newViewsCount = data.views + 1;
+
+      // Update the views count in the database
+      const { error: updateError } = await supabase
+        .from('links')
+        .update({ views: newViewsCount })
+        .eq('id', id)
+    
+      if (updateError) {
+        console.error('Error updating views count:', updateError)
+        return
+      }
+
+      
+      window.location.href = data.url
+    }
+    if (id) redirectToOriginalUrl()
+  }, [id])
+
+  return null
+}
+
+export default ShortUrlRedirect
